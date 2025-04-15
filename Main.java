450950dbed40facc8c1dc2246f98908ecb9d78d8
Bryan Strey <137365683+joystick2707@users.noreply.github.com>
2025-04-15 15:56:47 -0300

    Classe Main

--- Main.java
+++ Main.java
@@ -1,6 +1,6 @@
 import java.util.Scanner;
 
-public class RestauranteApp {
+public class Main {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
         Cardapio cardapio = new Cardapio();
