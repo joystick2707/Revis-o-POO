aab51eb4a051b5e8470dc3cc99d563c080b0b1de
Bryan Strey <137365683+joystick2707@users.noreply.github.com>
2025-04-15 15:56:03 -0300

    Create Cardapio.java

---
+++ Cardapio.java
@@ -0,0 +1,30 @@
+import java.util.ArrayList;
+import java.util.List;
+
+public class Cardapio {
+    private List<Item> itens = new ArrayList<>();
+
+    public Cardapio() {
+        itens.add(new Item(1, "Hamburguer", 20.0));
+        itens.add(new Item(2, "Batata Frita", 10.0));
+        itens.add(new Item(3, "Refrigerante", 5.0));
+        itens.add(new Item(4, "Suco Natural", 7.0));
+        itens.add(new Item(5, "Sobremesa", 12.0));
+    }
+
+    public void exibirCardapio() {
+        System.out.println("\n--- CARDÁPIO ---");
+        for (Item item : itens) {
+            System.out.println(item);
+        }
+    }
+
+    public Item buscarItemPorCodigo(int codigo) {
+        for (Item item : itens) {
+            if (item.getCodigo() == codigo) {
+                return item;
+            }
+        }
+        return null;
+    }
+}
