ddc880da680ebe74d38954018de9b37f8a5414ba
Bryan Strey <137365683+joystick2707@users.noreply.github.com>
2025-04-15 15:56:16 -0300

    Create Item.java

---
+++ Item.java
@@ -0,0 +1,28 @@
+public class Item {
+    private int codigo;
+    private String nome;
+    private double preco;
+
+    public Item(int codigo, String nome, double preco) {
+        this.codigo = codigo;
+        this.nome = nome;
+        this.preco = preco;
+    }
+
+    public int getCodigo() {
+        return codigo;
+    }
+
+    public String getNome() {
+        return nome;
+    }
+
+    public double getPreco() {
+        return preco;
+    }
+
+    @Override
+    public String toString() {
+        return codigo + " - " + nome + " R$ " + String.format("%.2f", preco);
+    }
+}
